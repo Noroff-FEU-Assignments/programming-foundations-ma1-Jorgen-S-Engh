308d9ac01cf8e7f167252280660c964c0f3077ec
Jorgen-S-Engh <55982088+Jorgen-S-Engh@users.noreply.github.com>
2022-01-07 10:57:46 +0100

    Modul 1 assignment- Jørgen Engh

---
+++ js/scipts.js
@@ -0,0 +1,34 @@
+// oppgave 1
+var number = 33;
+    boolean = true;
+    text = "Some text";
+
+// oppgave 2
+var firstName = "Jørgen";
+    lastName = "Engh";
+
+var fullName = firstName + " " + lastName;
+
+console.log(fullName)
+
+// oppgave 3
+var animal = (typeof("frog"));
+console.log("The type of frog is " + animal)
+
+// oppgave 4
+var orderHasShipped = true;
+
+if (orderHasShipped) {
+    console.log("The order has shipped");
+}
+else{
+    console.log("The order did not ship");
+}
+
+// oppgave 5
+
+var count =7
+
+for(count = 7; count <= 13; count++) {
+    console.log(count);
+}
